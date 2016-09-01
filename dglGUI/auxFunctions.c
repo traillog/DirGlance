4dae6304c5ef5536e46f4f373ed407d014c1b496
traillog <traillog.info@gmail.com>
2016-08-31 19:00:46 -0500

    Basic functionality added.
    - Basic functionality added.
    - GUI layout still raw.
    - No threads used.

---
+++ dglGUI/auxFunctions.c
@@ -0,0 +1,547 @@
+//
+// auxFunctions.c
+//
+// Auxiliary Functions Subdirectory Analysis
+
+#include <windows.h>
+#include <stdio.h>
+#include <wchar.h>
+#include "list.h"               // Definition list ADT
+
+#define     MAX_OPTIONS     20  // Max # command line options
+
+// Flags indices
+#define     FL_SIZE         0   // Sort by size [bytes] (default)
+#define     FL_FILES        1   // Sort by files count (descending)
+#define     FL_DIRS         2   // Sort by dirs count (descending)
+#define     FL_MODIF        3   // Sort by date modified (latest to earliest)
+#define     FL_NAME         4   // Soft by name (a to Z)
+#define     FL_TYPE         5   // Sort by type (<DIR>, <LIN>, file)
+#define     FL_HELP         6   // Print usage
+#define     FL_DBG          7   // Extended output (debug purposes)
+
+// Function prototypes
+void analyseSubDir( TCHAR* targetDir, HWND hContsLBox );
+extern DWORD Options( int argc, LPCWSTR argv[], LPCWSTR OptStr, ... );
+extern VOID ReportError( LPCTSTR userMsg, DWORD exitCode, BOOL prtErrorMsg );
+BOOL scanDir( LPTSTR tDir, List* resList, Item* parentItem, BOOL fstLevel, BOOL dbg );
+BOOL ptDir( LPCTSTR inDir );
+void showResults( List* resultsList, Item* resultsLevel, HWND hConstLBox );
+void showItem( Item* pItem, HWND hConstLBox );
+int cmpItemsLastWriteTime( Item* pItemN, Item* pItemM );
+int cmpItemsDirsCount( Item* pItemN, Item* pItemM );
+int cmpItemsFilesCount( Item* pItemN, Item* pItemM );
+int cmpItemsSizeCount( Item* pItemN, Item* pItemM );
+int cmpItemsName( Item* pItemN, Item* pItemM );
+void calcDispElapTime( const long long* ticksPt, const long long* freqPt );
+void sepThousands( const long long* numPt, TCHAR* acc, size_t elemsAcc );
+
+void analyseSubDir( TCHAR* targetDir, HWND hContsLBox )
+{
+    // Declare vars
+    Item resultsItem = { 0 };
+    List resultsList = { 0 };
+    BOOL flags[ MAX_OPTIONS ] = { 0 };
+    PVOID oldValueWow64 = NULL;
+    BOOL wow64Disabled = FALSE;
+
+    // Initialize results list
+    InitializeList( &resultsList );
+    if ( ListIsFull( &resultsList ) )
+    {
+        wprintf_s( TEXT( "\nNo memory available!\n" ) );
+        return;
+    }
+
+    // Disable file system redirection
+    wow64Disabled = Wow64DisableWow64FsRedirection( &oldValueWow64 );
+
+    // Scan target dir
+    scanDir( targetDir, &resultsList, &resultsItem, TRUE, flags[ FL_DBG ] );
+
+    // Re-enable redirection
+    if ( wow64Disabled )
+    {
+        if ( !( Wow64RevertWow64FsRedirection( oldValueWow64 ) ) )
+            ReportError( TEXT( "Re-enable redirection failed." ), 1, TRUE );
+    }
+
+    // Display results
+    if ( ListIsEmpty( &resultsList ) )
+        wprintf_s( TEXT( "\nNo data.\n\n" ) );
+    else
+    {
+        // Sort results
+        // if-else chain determines sorting priority
+        // one sorting type high prio excludes low prio types
+        if ( flags[ FL_SIZE ] )
+            // Sort by size (descending)
+            SortList( &resultsList, cmpItemsSizeCount );
+        else if ( flags[ FL_FILES ] )
+            // Sort by files count (descending)
+            SortList( &resultsList, cmpItemsFilesCount );
+        else if ( flags[ FL_DIRS ] )
+            // Sort by dirs count (descending)
+            SortList( &resultsList, cmpItemsDirsCount );
+        else if ( flags[ FL_MODIF ] )
+            // Sort by modification date (latest to earliest)
+            SortList( &resultsList, cmpItemsLastWriteTime );
+        else if ( flags[ FL_NAME ] )
+            // Sort by name (a to Z)
+            SortList( &resultsList, cmpItemsName );
+        else
+            // Default: sort by size (descending)
+            SortList( &resultsList, cmpItemsSizeCount );
+
+        // Debug output
+        if ( flags[ FL_DBG ] )
+            wprintf_s( TEXT( "\n" ) );
+
+        // Display sorted results
+        showResults( &resultsList, &resultsItem, hContsLBox );
+    }
+
+    // Housekeeping
+    EmptyTheList( &resultsList );
+}
+
+BOOL scanDir( LPTSTR tDir, List* resList, Item* parentItem, BOOL fstLevel, BOOL dbg )
+{
+    TCHAR dirStr[ MAX_PATH ] = { 0 };
+    HANDLE hFind = INVALID_HANDLE_VALUE;
+
+    Item currentItem = { 0 };
+    
+    LARGE_INTEGER parentSize = { 0 };
+    LARGE_INTEGER currentSize = { 0 };
+
+    // Prepare string for use with FindFile functions.  First, copy the
+    // string to a buffer, then append '\*' to the directory name.
+    wcscpy_s( dirStr, MAX_PATH, tDir );
+
+    if ( dirStr[ wcslen( dirStr ) - 1 ] == TEXT( '\\' ) )
+        wcscat_s( dirStr, MAX_PATH, TEXT( "*" ) );
+    else
+        wcscat_s( dirStr, MAX_PATH, TEXT( "\\*" ) );
+
+    // Find the first file in the directory.
+    hFind = FindFirstFile( dirStr, &currentItem.findInfo );
+
+    // Validate search handle
+    if ( INVALID_HANDLE_VALUE == hFind )
+    {
+        // Only report error if different from 'Access Denied'.
+        // For example, system symbolic links report 'access denied'.
+        // If a handle is obtained and the size is requested,
+        // Win32 reports 0 bytes.
+        // See results using '..\progsDev\others\TestGetFileSizeEx\'
+        if ( GetLastError() != ERROR_ACCESS_DENIED )
+            ReportError( TEXT( "FindFirstFile failed." ), 0, TRUE );
+
+        // Exit in any case
+        return FALSE;
+    }
+
+    // List all the files/subdirs in the directory
+    do
+    {
+        // Do not follow symbolic links
+        // Symbolic links are listed but,
+        // they do not affect dirs nor files count
+        // Their size (0 bytes) and date are taken into account
+        if ( !( currentItem.findInfo.dwFileAttributes &
+                FILE_ATTRIBUTE_REPARSE_POINT ) )
+        {
+            // Subdirectory found ?
+            if ( currentItem.findInfo.dwFileAttributes &
+                 FILE_ATTRIBUTE_DIRECTORY )
+            {
+                // Dir found
+
+                // Skip '.' or '..'
+                if ( ptDir( currentItem.findInfo.cFileName ) )
+                    continue;
+
+                // Increment dirs count of parent
+                ++parentItem->dirsCount.QuadPart;
+
+                // Prepare the recursive call
+                wcscpy_s( dirStr, MAX_PATH, tDir );
+                wcscat_s( dirStr, MAX_PATH, TEXT( "\\" ) );
+                wcscat_s( dirStr, MAX_PATH, currentItem.findInfo.cFileName );
+
+                // Debug output
+                if ( dbg )
+                    wprintf_s( TEXT( "    %s\n" ), dirStr );
+
+                // Scan subdir
+                scanDir( dirStr, resList, &currentItem, FALSE, dbg );
+
+                // Update dirs count of parent using found subdirs
+                parentItem->dirsCount.QuadPart +=
+                    currentItem.dirsCount.QuadPart;
+
+                // Update files count of parent using found files
+                parentItem->filesCount.QuadPart +=
+                    currentItem.filesCount.QuadPart;
+            }
+            else
+            {
+                // File found
+
+                // Increment files count of parent
+                ++parentItem->filesCount.QuadPart;
+
+                // Current item has one file
+                currentItem.filesCount.QuadPart = 1;
+            }
+        }
+
+        // Update last write time information of the parent.
+        // Is current item's LastWriteTime later
+        // than parent's LastWriteTime ?
+        if ( CompareFileTime( &currentItem.findInfo.ftLastWriteTime,
+            &parentItem->findInfo.ftLastWriteTime ) == 1 )
+        {
+            // Parent gets the LastWriteTime from current item
+            parentItem->findInfo.ftLastWriteTime =
+                currentItem.findInfo.ftLastWriteTime;
+        }
+
+        // Get size of current found file(s)
+        currentSize.LowPart = currentItem.findInfo.nFileSizeLow;
+        currentSize.HighPart = currentItem.findInfo.nFileSizeHigh;
+
+        // Get size of parent so far
+        parentSize.LowPart = parentItem->findInfo.nFileSizeLow;
+        parentSize.HighPart = parentItem->findInfo.nFileSizeHigh;
+
+        // Add current size to parent size (64-bit addition)
+        parentSize.QuadPart += currentSize.QuadPart;
+
+        // Update parent file size
+        parentItem->findInfo.nFileSizeLow = parentSize.LowPart;
+        parentItem->findInfo.nFileSizeHigh = parentSize.HighPart;
+
+        // Update list
+        if ( fstLevel )
+        {
+            // Append current item to results list
+            if ( AddItem( currentItem, resList ) == false )
+            {
+                wprintf_s( TEXT( "Problem allocating memory\n" ) );
+                return FALSE;
+            }
+        }
+
+        // Reset current item
+        memset( &currentItem, 0, sizeof( Item ) );
+
+    } while ( FindNextFile( hFind, &currentItem.findInfo ) != 0 );
+
+    // Validate end of search
+    if ( GetLastError() != ERROR_NO_MORE_FILES )
+    {
+        ReportError( TEXT( "\nFindNextFile failed.\n" ), 0, TRUE );
+        return FALSE;
+    }
+
+    // Close search handle
+    FindClose( hFind );
+
+    return TRUE;
+}
+
+BOOL ptDir( LPCTSTR inDir )
+{
+    if ( ( ( wcslen( inDir ) == 1 ) &&
+           ( wcscmp( inDir, TEXT( "." ) ) == 0 ) ) ||
+         ( ( wcslen( inDir ) == 2 ) &&
+           ( wcscmp( inDir, TEXT( ".." ) ) == 0 ) ) )
+    {
+        // inDir == "." || inDir == ".."
+        return TRUE;
+    }
+    else
+    {
+        // otherwise
+        return FALSE;
+    }
+}
+
+void showResults( List* resultsList, Item* resultsLevel, HWND hConstLBox )
+{
+    // Display header
+    wprintf_s( TEXT( "    %19s %5s %10s %10s %18s %s\n" ),
+        TEXT( "Date Modified" ),
+        TEXT( "Type" ),
+        TEXT( "Dirs" ),
+        TEXT( "Files" ),
+        TEXT( "Size" ),
+        TEXT( "Name" ) );
+
+    wprintf_s( TEXT( "    %19s %5s %10s %10s %18s %s\n" ),
+        TEXT( "-------------------" ),
+        TEXT( "-----" ),
+        TEXT( "----------" ),
+        TEXT( "----------" ),
+        TEXT( "------------------" ),
+        TEXT( "---------------------------------------------" ) );
+
+    // Display founded entries
+    Traverse( resultsList, showItem, hConstLBox );
+
+    // Display totals
+    wprintf_s( TEXT( "    %19s %5s %10s %10s %18s %s\n" ),
+        TEXT( "-------------------" ),
+        TEXT( "-----" ),
+        TEXT( "----------" ),
+        TEXT( "----------" ),
+        TEXT( "------------------" ),
+        TEXT( "---------------------------------------------" ) );
+
+    showItem( resultsLevel, hConstLBox );
+}
+
+void showItem( Item* pItem, HWND hContsLBox )
+{
+    FILETIME lastWriteFTIME;
+    SYSTEMTIME lastWriteSYSTIME;
+    LARGE_INTEGER entrySize;
+    static TCHAR dirsCtStr[ 32 ] = { 0 };
+    static TCHAR filesCtStr[ 32 ] = { 0 };
+    static TCHAR sizeStr[ 32 ] = { 0 };
+    static TCHAR dateStr[ 32 ] = { 0 };
+    static TCHAR typeStr[ 32 ] = { 0 };
+    static TCHAR detailsStr[ 512 ] = { 0 };
+    static TCHAR outStr[ 512 ] = { 0 };
+
+    // Fetch and prepare entry last modification date & time
+
+    // UTC time (FILETIME) to local time (FILETIME)
+    FileTimeToLocalFileTime( &pItem->findInfo.ftLastWriteTime,
+        &lastWriteFTIME );
+
+    // local time (FILETIME) to local time (SYSTIME)
+    FileTimeToSystemTime( &lastWriteFTIME, &lastWriteSYSTIME );
+
+    // Disp entry last modification date & time
+    swprintf_s( dateStr, _countof( dateStr ),
+        TEXT( "    %04d-%02d-%02d %02d:%02d:%02d" ),
+        lastWriteSYSTIME.wYear,
+        lastWriteSYSTIME.wMonth,
+        lastWriteSYSTIME.wDay,
+        lastWriteSYSTIME.wHour,
+        lastWriteSYSTIME.wMinute,
+        lastWriteSYSTIME.wSecond );
+
+    // Disp entry type (dir/file)
+    if ( pItem->findInfo.dwFileAttributes & FILE_ATTRIBUTE_REPARSE_POINT )
+        swprintf_s( typeStr, _countof( typeStr ),
+            TEXT( " %5s" ), TEXT( "<LIN>" ) );
+    else if ( pItem->findInfo.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY )
+        swprintf_s( typeStr, _countof( typeStr ),
+            TEXT( " %5s" ), TEXT( "<DIR>" ) );
+    else
+        swprintf_s( typeStr, _countof( typeStr ),
+            TEXT( " %5s" ), TEXT( "     " ) );
+
+    // Fetch entry size
+    entrySize.LowPart = pItem->findInfo.nFileSizeLow;
+    entrySize.HighPart = pItem->findInfo.nFileSizeHigh;
+
+    // Convert nums to strings (thousands separated)
+    sepThousands( &pItem->dirsCount.QuadPart, dirsCtStr,
+        _countof( dirsCtStr ) );
+    sepThousands( &pItem->filesCount.QuadPart, filesCtStr,
+        _countof( filesCtStr ) );
+    sepThousands( &entrySize.QuadPart, sizeStr,
+        _countof( sizeStr ) );
+
+    // Disp entry details
+    swprintf_s( detailsStr, _countof( detailsStr ),
+        TEXT( " %10s %10s %18s %s\n" ),
+        dirsCtStr,
+        filesCtStr,
+        sizeStr,
+        pItem->findInfo.cFileName );
+
+    // Ensamble output line
+    wcscpy_s( outStr, _countof( outStr ), dateStr );
+    wcscat_s( outStr, _countof( outStr ), typeStr );
+    wcscat_s( outStr, _countof( outStr ), detailsStr );
+
+    // Append output line to target listbox
+    SendMessage( hContsLBox, LB_ADDSTRING, 0, ( LPARAM )outStr );
+}
+
+// compares LastWriteTime of two items
+//   1 : item N later than item M
+//   0 : item N same time item M
+//  -1 : item N earlier item M
+int cmpItemsLastWriteTime( Item* pItemN, Item* pItemM )
+{
+    return CompareFileTime( &pItemN->findInfo.ftLastWriteTime,
+        &pItemM->findInfo.ftLastWriteTime );
+}
+
+// compares dirs count of two items
+//   1 : item N > item M
+//   0 : item N = item M
+//  -1 : item N < item M
+int cmpItemsDirsCount( Item* pItemN, Item* pItemM )
+{
+    // Def vars
+    LARGE_INTEGER valN = { 0 };
+    LARGE_INTEGER valM = { 0 };
+
+    // Fetch N value
+    valN.HighPart = pItemN->dirsCount.HighPart;
+    valN.LowPart = pItemN->dirsCount.LowPart;
+
+    // Fetch M value
+    valM.HighPart = pItemM->dirsCount.HighPart;
+    valM.LowPart = pItemM->dirsCount.LowPart;
+
+    // Perform 64-bit comparison
+    if ( valN.QuadPart > valM.QuadPart )
+        return 1;
+    else if ( valN.QuadPart == valM.QuadPart )
+        return 0;
+    else
+        return -1;
+}
+
+// compares files count of two items
+//   1 : item N > item M
+//   0 : item N = item M
+//  -1 : item N < item M
+int cmpItemsFilesCount( Item* pItemN, Item* pItemM )
+{
+    // Def vars
+    LARGE_INTEGER valN = { 0 };
+    LARGE_INTEGER valM = { 0 };
+
+    // Fetch N value
+    valN.HighPart = pItemN->filesCount.HighPart;
+    valN.LowPart = pItemN->filesCount.LowPart;
+
+    // Fetch M value
+    valM.HighPart = pItemM->filesCount.HighPart;
+    valM.LowPart = pItemM->filesCount.LowPart;
+
+    // Perform 64-bit comparison
+    if ( valN.QuadPart > valM.QuadPart )
+        return 1;
+    else if ( valN.QuadPart == valM.QuadPart )
+        return 0;
+    else
+        return -1;
+}
+
+// compares size count of two items
+//   1 : item N > item M
+//   0 : item N = item M
+//  -1 : item N < item M
+int cmpItemsSizeCount( Item* pItemN, Item* pItemM )
+{
+    // Def vars
+    LARGE_INTEGER valN = { 0 };
+    LARGE_INTEGER valM = { 0 };
+
+    // Fetch N value
+    valN.HighPart = pItemN->findInfo.nFileSizeHigh;
+    valN.LowPart = pItemN->findInfo.nFileSizeLow;
+
+    // Fetch M value
+    valM.HighPart = pItemM->findInfo.nFileSizeHigh;
+    valM.LowPart = pItemM->findInfo.nFileSizeLow;
+
+    // Perform 64-bit comparison
+    if ( valN.QuadPart > valM.QuadPart )
+        return 1;
+    else if ( valN.QuadPart == valM.QuadPart )
+        return 0;
+    else
+        return -1;
+}
+
+// compare names of two items
+//  >0 : item N before item M
+//   0 : item N same place as item M
+//  <0 : item N after item M
+int cmpItemsName( Item* pItemN, Item* pItemM )
+{
+    int result;
+
+    // case-insensitive comparison
+    result = _wcsicmp( pItemM->findInfo.cFileName, pItemN->findInfo.cFileName );
+
+    return result;
+}
+
+void calcDispElapTime( const long long* ticksPt, const long long* freqPt )
+{
+    unsigned long long totalMicros = 0, restMicros = 0;
+    unsigned long long millis = 0, secs = 0, mins = 0;
+    
+    totalMicros = *ticksPt * 1000000 / *freqPt;
+
+    millis = totalMicros / 1000;        // total millis
+
+    restMicros = totalMicros % 1000;
+
+    if ( restMicros >= 500 )            // round to millis
+        ++millis;
+
+    secs = millis / 1000;       // total secs
+
+    millis = millis % 1000;     // rest millis
+
+    mins = secs / 60;           // total mins
+
+    secs = secs % 60;           // rest secs
+
+    // Display results
+    wprintf( TEXT( "\n    Elapsed time [mm:ss.mil] : " ) );
+    wprintf( TEXT( "%02I64d:%02I64d.%03I64d\n" ),
+        mins, secs, millis );
+}
+
+void sepThousands( const long long* numPt, TCHAR* acc, size_t elemsAcc )
+{
+    static TCHAR app[ 32 ] = { 0 }; // append data (last three digits as chars)
+    long long numIn = *numPt;       // remaining num
+    long long res = 0;              // append data (last three digits as nums)    
+
+    // Reset accumulated result (output str)
+    wmemset( acc, 0, elemsAcc );
+
+    // Iterate as long as there are digits left
+    if ( numIn == 0 )
+        wcscpy_s( acc, elemsAcc, TEXT( "0" ) );
+    else
+    {
+        while ( numIn > 0 )
+        {
+            // Reset append storage
+            wmemset( app, 0, _countof( app ) );
+
+            // Get three right most digits
+            res = numIn % 1000;
+
+            // Get the remaining num
+            numIn = numIn / 1000;
+
+            // Build up string to be appended in front of accumulated result
+            swprintf_s( app, _countof( app ),
+                ( numIn > 0 ) ? TEXT( ",%03d" ) : TEXT( "%3d" ), res );
+
+            // Append accumulated result to new digits
+            wcscat_s( app, _countof( app ), acc );
+
+            // Update accumulated result
+            wcscpy_s( acc, elemsAcc, app );
+        }
+    }
+}
--- dglGUI/dglGUI.c
+++ dglGUI/dglGUI.c
@@ -5,6 +5,19 @@
 
 #include <windows.h>
 
+#define     ID_CurLocLbl            1
+#define     ID_CurDirLBox           2
+#define     ID_PlacesLbl            3
+#define     ID_PlacesLBox           4
+#define     ID_ContsLbl             5
+#define     ID_ContsLBox            6
+#define     ID_SortLbl              7
+#define     ID_NameBtn              8
+#define     ID_SizeBtn              9
+#define     ID_DateBtn              10
+
+extern void analyseSubDir( TCHAR* targetDir, HWND hContsLBox );
+
 LRESULT CALLBACK WndProc( HWND, UINT, WPARAM, LPARAM );
 
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
@@ -38,7 +51,7 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
         TEXT( "Dir Glance v0.1" ),
         WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
         CW_USEDEFAULT, CW_USEDEFAULT,
-        600, 500,
+        1100, 640,
         NULL,
         NULL,
         hInstance,
@@ -59,19 +72,234 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
 LRESULT CALLBACK WndProc( HWND hwnd, UINT message,
     WPARAM wParam, LPARAM lParam )
 {
+    static HWND hCurLocLbl, hCurDirLBox;
+    static HWND hPlacesLbl, hPlacesLBox;
+    static HWND hContsLbl, hContsLBox;
+    static HWND hSortLbl, hNameBtn, hSizeBtn, hDateBtn;
+    static int cxChar, cyChar;
+    static int cxClient, cyClient;
+    TCHAR currentDir[ MAX_PATH + 1 ] = { 0 };
+    TCHAR selSubDir[ MAX_PATH + 1 ] = { 0 };
+    int currentSelIndex;
+    static LOGFONT logfont;
+    static HFONT hFont;
+
     switch ( message )
     {
-    case WM_KEYDOWN :
-        switch ( wParam )
-        {
-        case VK_ESCAPE :
-            // Close the application
-            DestroyWindow( hwnd );
-            return 0;
+    case WM_CREATE :
+        // Get char metrics --> used as a basic children alignment unit
+        cxChar = LOWORD( GetDialogBaseUnits() );
+        cyChar = HIWORD( GetDialogBaseUnits() );
 
-        default :
-            return 0;
+        // Create 'Current Location' label
+        hCurLocLbl = CreateWindow(
+            TEXT( "static" ), TEXT( "Current Location :" ),
+            WS_CHILD | WS_VISIBLE | SS_LEFT,
+            0, 0, 0, 0,
+            hwnd, ( HMENU )( ID_CurLocLbl ),
+            ( ( LPCREATESTRUCT )lParam )->hInstance, NULL );
+
+        // Create 'Current Dir' list box
+        hCurDirLBox = CreateWindow(
+            TEXT( "listbox" ), NULL,
+            WS_CHILD | WS_VISIBLE | LBS_STANDARD | WS_HSCROLL,
+            0, 0, 0, 0,
+            hwnd, ( HMENU )( ID_CurDirLBox ),
+            ( ( LPCREATESTRUCT )lParam )->hInstance, NULL );
+
+        // Create 'Places' label
+        hPlacesLbl = CreateWindow(
+            TEXT( "static" ), TEXT( "Places :" ),
+            WS_CHILD | WS_VISIBLE | SS_LEFT,
+            0, 0, 0, 0,
+            hwnd, ( HMENU )( ID_PlacesLbl ),
+            ( ( LPCREATESTRUCT )lParam )->hInstance, NULL );
+
+        // Create 'Places' list box
+        hPlacesLBox = CreateWindow(
+            TEXT( "listbox" ), NULL,
+            WS_CHILD | WS_VISIBLE | LBS_STANDARD | WS_HSCROLL,
+            0, 0, 0, 0,
+            hwnd, ( HMENU )( ID_PlacesLBox ),
+            ( ( LPCREATESTRUCT )lParam )->hInstance, NULL );
+
+        // Create 'Contents' label
+        hContsLbl = CreateWindow(
+            TEXT( "static" ), TEXT( "Contents :" ),
+            WS_CHILD | WS_VISIBLE | SS_LEFT,
+            0, 0, 0, 0,
+            hwnd, ( HMENU )( ID_ContsLbl ),
+            ( ( LPCREATESTRUCT )lParam )->hInstance, NULL );
+
+        // Create 'Contents' list box
+        hContsLBox = CreateWindow(
+            TEXT( "listbox" ), NULL,
+            WS_CHILD | WS_VISIBLE | LBS_NOTIFY | WS_BORDER |
+                WS_VSCROLL | WS_HSCROLL,
+            0, 0, 0, 0,
+            hwnd, ( HMENU )( ID_ContsLBox ),
+            ( ( LPCREATESTRUCT )lParam )->hInstance, NULL );
+
+        // Create 'Sort' label
+        hSortLbl = CreateWindow(
+            TEXT( "static" ), TEXT( "Sort :" ),
+            WS_CHILD | WS_VISIBLE | SS_LEFT,
+            0, 0, 0, 0,
+            hwnd, ( HMENU )( ID_SortLbl ),
+            ( ( LPCREATESTRUCT )lParam )->hInstance, NULL );
+
+        // Create 'By Name' button
+        hNameBtn = CreateWindow(
+            TEXT( "button" ), TEXT( "By Name" ),
+            WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+            0, 0, 0, 0,
+            hwnd, ( HMENU )( ID_NameBtn ),
+            ( ( LPCREATESTRUCT )lParam )->hInstance, NULL );
+
+        // Create 'By Size' button
+        hSizeBtn = CreateWindow(
+            TEXT( "button" ), TEXT( "By Size" ),
+            WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+            0, 0, 0, 0,
+            hwnd, ( HMENU )( ID_SizeBtn ),
+            ( ( LPCREATESTRUCT )lParam )->hInstance, NULL );
+
+        // Create 'By Date' button
+        hDateBtn = CreateWindow(
+            TEXT( "button" ), TEXT( "By Date" ),
+            WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+            0, 0, 0, 0,
+            hwnd, ( HMENU )( ID_DateBtn ),
+            ( ( LPCREATESTRUCT )lParam )->hInstance, NULL );
+
+        // Initialize font of the 'Contents' listbox
+        GetObject( GetStockObject( SYSTEM_FIXED_FONT ), sizeof( LOGFONT ), 
+            ( PTSTR )&logfont );
+        hFont = CreateFontIndirect( &logfont );
+        SendMessage( hContsLBox, WM_SETFONT, ( WPARAM )hFont, 0 );
+
+        // Setup listboxes horizontal extents
+        SendMessage( hCurDirLBox, LB_SETHORIZONTALEXTENT, 1000, 0 );
+        SendMessage( hPlacesLBox, LB_SETHORIZONTALEXTENT, 1000, 0 );
+        SendMessage( hContsLBox, LB_SETHORIZONTALEXTENT, 1000, 0 );
+
+        // Get current dir
+        GetCurrentDirectory( MAX_PATH + 1, currentDir );
+
+        // Show current dir in list box
+        SendMessage( hCurDirLBox, LB_ADDSTRING, 0, ( LPARAM )currentDir );
+
+        // Populate the 'Places' list box
+        SendMessage( hPlacesLBox, LB_DIR,
+            DDL_DRIVES | DDL_DIRECTORY | DDL_EXCLUSIVE,
+            ( LPARAM )TEXT( "*" ) );
+
+        // Update 'Contents' list box
+        analyseSubDir( currentDir, hContsLBox );
+
+        return 0;
+
+    case WM_SIZE :
+        // Get client size
+        cxClient = LOWORD( lParam );
+        cyClient = HIWORD( lParam );
+
+        // Size and position 'Current Location' label
+        MoveWindow( hCurLocLbl,
+            cyChar, cyChar,
+            24 * cxChar, cyChar, TRUE);
+
+        // Size and position 'Current Dir' list box
+        MoveWindow( hCurDirLBox,
+            cyChar, 2 * cyChar,
+            cxClient - 3 * cyChar, 3 * cyChar, TRUE);
+
+        // Size and position 'Places' label
+        MoveWindow( hPlacesLbl,
+            cyChar, 5 * cyChar,
+            24 * cxChar, cyChar, TRUE);
+
+        // Size and position 'Places' list box
+        MoveWindow( hPlacesLBox,
+            cyChar, 6 * cyChar,
+            30 * cxChar, cyClient - 7 * cyChar, TRUE);
+
+        // Size and position 'Contents' label
+        MoveWindow( hContsLbl,
+            cyChar + 30 * cxChar + cyChar, 5 * cyChar,
+            24 * cxChar, cyChar, TRUE);
+
+        // Size and position 'Contents' list box
+        MoveWindow( hContsLBox,
+            cyChar + 30 * cxChar + cyChar, 6 * cyChar,
+            cxClient - ( cyChar + 30 * cxChar + cyChar + cyChar),
+            cyClient - 12 * cyChar, TRUE);
+
+        // Size and position 'Sort' label
+        MoveWindow( hSortLbl,
+            cyChar + 30 * cxChar + cyChar, cyClient - 5 * cyChar,
+            24 * cxChar, cyChar, TRUE);
+
+        // Size and position 'By Name' button
+        MoveWindow( hNameBtn,
+            cyChar + 30 * cxChar + cyChar, cyClient - 4 * cyChar,
+            12 * cxChar, 2 * cyChar, TRUE);
+
+        // Size and position 'By Size' button
+        MoveWindow( hSizeBtn,
+            cyChar + 30 * cxChar + cyChar + 13 * cxChar, cyClient - 4 * cyChar,
+            12 * cxChar, 2 * cyChar, TRUE);
+
+        // Size and position 'By Date' button
+        MoveWindow( hDateBtn,
+            cyChar + 30 * cxChar + cyChar + 13 * cxChar + 13 * cxChar,
+            cyClient - 4 * cyChar,
+            12 * cxChar, 2 * cyChar, TRUE);
+
+        return 0;
+
+    case WM_COMMAND :
+        if ( LOWORD( wParam ) == ID_PlacesLBox &&
+             HIWORD( wParam ) == LBN_DBLCLK )
+        {
+            if ( LB_ERR == ( currentSelIndex =
+                SendMessage( hPlacesLBox, LB_GETCURSEL, 0, 0 ) ) )
+                break;
+
+            SendMessage( hPlacesLBox, LB_GETTEXT, currentSelIndex,
+                ( LPARAM )selSubDir );
+
+            selSubDir[ lstrlen( selSubDir ) - 1 ] = '\0';
+
+            // Try to go to selected subdirectory,
+            // if that doesn't work it has to be a drive
+            if ( !SetCurrentDirectory( selSubDir + 1 ) )
+            {
+                selSubDir[ 3 ] = ':';
+                selSubDir[ 4 ] = '\0';
+                SetCurrentDirectory( selSubDir + 2 );
+            }
+
+            // Get the new directory name and fill the list box.
+            GetCurrentDirectory( MAX_PATH + 1, currentDir );
+
+            // Update 'Current Dir' list box
+            SendMessage( hCurDirLBox, LB_RESETCONTENT, 0, 0 );
+            SendMessage( hCurDirLBox, LB_ADDSTRING, 0, ( LPARAM )currentDir );
+
+            // Reload the 'Places' list box
+            SendMessage( hPlacesLBox, LB_RESETCONTENT, 0, 0 );
+            SendMessage( hPlacesLBox, LB_DIR,
+                DDL_DRIVES | DDL_DIRECTORY | DDL_EXCLUSIVE,
+                ( LPARAM )TEXT( "*" ) );
+
+            // Update 'Contents' list box
+            SendMessage( hContsLBox, LB_RESETCONTENT, 0, 0 );
+            analyseSubDir( currentDir, hContsLBox );
+
+            InvalidateRect (hwnd, NULL, TRUE) ;
         }
+        return 0;
 
     case WM_DESTROY :
         // Close the application
